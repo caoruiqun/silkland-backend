ed293661a921fc1aa3a0760a8ae6636227b9206a
caoruiqun <caoruiqun999@163.com>
2023-06-21 15:24:52 +0800

    template工程更名为test工程

--- springboot-template-project/src/main/resources/sql/table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `t_user_info` (
-   `id` int NOT NULL AUTO_INCREMENT,
-   `name` varchar(10) NOT NULL,
-   `age` int DEFAULT NULL,
-   `create_time` datetime DEFAULT NULL,
-   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
