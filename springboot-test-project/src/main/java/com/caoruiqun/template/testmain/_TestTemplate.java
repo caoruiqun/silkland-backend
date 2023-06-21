61bf83de04a92d622115beaff6b908297eb9a118
caoruiqun <caoruiqun999@163.com>
2023-06-21 15:49:53 +0800

    unit test template

---
+++ springboot-test-project/src/main/java/com/caoruiqun/template/testmain/_TestTemplate.java
@@ -0,0 +1,21 @@
+package com.caoruiqun.template.testmain;
+
+
+import org.junit.Test;
+
+/**
+ * @author caoruiqun
+ * @date 2023/6/6 16:19
+ * @desc:
+ */
+public class _TestTemplate {
+
+    /**
+     * xxx
+     */
+    @Test
+    public void test00() {
+
+    }
+
+}
